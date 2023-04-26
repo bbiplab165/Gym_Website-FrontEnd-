069d28ceec9a611e889d609fe517cf05641d0f57
bbiplab165 <bbiplab165@gmail.com>
2023-04-26 15:15:17 +0530

    Added

--- src/pages/training/Training.js
+++ src/pages/training/Training.js
@@ -9,7 +9,7 @@ const Training = () => {
       url: 'https://img2.wallspic.com/previews/1/0/0/1/31001/31001-bodybuilder-arm-strength_training-male-fitness_centre-x750.jpg',
       name: 'Strngth Training'
     },
-    
+
     {
       url: 'https://media.istockphoto.com/id/655781960/photo/determined-male-working-out-in-gym-lifting-weights.jpg?s=612x612&w=0&k=20&c=k9C0fgbVHYxiQLe8hckSx9OqAEaGVUfiVXYq8JGxhoE=',
       name: 'lio'
@@ -28,36 +28,36 @@ const Training = () => {
     }
   ]
   const [index, setIndex] = useState(0);
-  const length = photos.length ;
-  
+  const length = photos.length;
+
 
   const handlePrevious = () => {
-    setIndex(index>0 ? index-1 : length-1 )
+    setIndex(index > 0 ? index - 1 : length - 1)
   };
 
   const handleNext = () => {
     // const newIndex = index + 1;
     // setIndex(newIndex >= length ? 0 : newIndex);
-    setIndex(index<length-1 ? index+1 : 0)
-    
+    setIndex(index < length - 1 ? index + 1 : 0)
+
   };
 
   return (
     <>
-    <Navbar />
-    <div className={Style.container}>
-    
-      <div className={Style.carousel}>
-        <button onClick={handlePrevious}><FaLessThan /></button>
-        <div className={Style.imgContainer}>
-          <img src={photos[index].url} alt={photos[index].name} />
-          <p>{photos[index].name}</p>
+      <Navbar />
+      <div className={Style.container}>
+
+        <div className={Style.carousel}>
+          <button onClick={handlePrevious}><FaLessThan /></button>
+          <div className={Style.imgContainer}>
+            <img src={photos[index].url} alt={photos[index].name} />
+            <p>{photos[index].name}</p>
+          </div>
+          <button onClick={handleNext}><FaGreaterThan /></button>
         </div>
-        <button onClick={handleNext}><FaGreaterThan /></button>
+
       </div>
-       
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
